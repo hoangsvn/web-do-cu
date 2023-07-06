ea9a46414801ab9e4963415f4bab059293fbb2a9
Nguyễn Xuân Hoàng <nguyenxuanhoang02042001@gmail.com>
2023-07-06 21:16:01 +0700

    ups

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Compoment/Home/Home';
+import Login from './Compoment/Login/Login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home/>} ></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' ></Route>
+        <Route path='/customer'></Route>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
