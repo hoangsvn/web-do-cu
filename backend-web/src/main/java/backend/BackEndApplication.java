943715560cd775e0cd410aadd9ea922c99a3c995
Nguyễn Xuân Hoàng <nguyenxuanhoang02042001@gmail.com>
2023-08-17 00:21:42 +0700

    build tat test di

--- backend-web/src/main/java/backend/BackEndApplication.java
+++ backend-web/src/main/java/backend/BackEndApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
 @ComponentScan(basePackages = "backend")
 @EnableJpaRepositories(basePackages = "backend.repository")
+@SpringBootApplication
 public class BackEndApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackEndApplication.class, args);
